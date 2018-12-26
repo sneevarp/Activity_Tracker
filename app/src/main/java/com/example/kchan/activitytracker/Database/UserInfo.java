f2b555c31970d24721d7898cd5601ff46842a0b0
mohanbalaji95 <mohanbalaji95@yahoo.com>
2018-12-26 11:51:11 -0800

    UserDb and Userinfo inclusion

---
+++ app/src/main/java/com/example/kchan/activitytracker/Database/UserInfo.java
@@ -0,0 +1,31 @@
+package com.example.kchan.activitytracker.Database;
+
+public class UserInfo {
+    private String name;
+    private String email;
+
+    public UserInfo()
+    {
+
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+}
